aec3b941bb447af404f800f5e42f286cb7ba929a
Darius Harrison <dharr2@monsanto.com>
2017-08-25 15:00:45 -0500

    screw you

---
+++ Mobster/Mobster WatchKit App/Home2InterfaceController.swift
@@ -0,0 +1,13 @@
+//
+//  Home2InterfaceController.swift
+//  Mobster
+//
+//  Created by HARRISON, DARIUS [AG/1000] on 8/25/17.
+//  Copyright © 2017 HARRISON, DARIUS [AG/1000]. All rights reserved.
+//
+
+import UIKit
+
+class Home2InterfaceController: WKInterfaceController {
+
+}
